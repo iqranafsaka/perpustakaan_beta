b06dc7d9980fbce293929b710ca3c4fac6536486
iqranafsaka <iqrashadow123@gmail.com>
2019-08-01 12:46:12 +0700

    Penambahan @Guest pada app.blade.php ulang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,7 @@
                             </li>
                         @endguest
                     </ul>
+                    
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
