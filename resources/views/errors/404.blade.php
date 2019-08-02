b7a578443851731c5263e8b47c68f0cf1b854d70
iqranafsaka <iqrashadow123@gmail.com>
2019-08-02 09:09:24 +0700

    Fixing 404 errors

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -12,11 +12,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>404 Error</title>
+    <title>404-Page not Found!</title>
 </head>
 <body>
-
-        <img src="{{asset('custom_img/GFL_404error.gif')}}" alt="Paris" class="center">
+        <br><br><br>
+        <img src="{{asset('custom_img/GFL_404error.gif')}}" alt="image" class="center" style="width:28%; height:auto;">
     
 </body>
 </html>
