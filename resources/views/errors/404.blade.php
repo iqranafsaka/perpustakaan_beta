747e109a116b2d29b2b89b8fba65cb3b9c0ea01c
iqranafsaka <iqrashadow123@gmail.com>
2019-08-02 08:59:32 +0700

    Penambahan Forgot Password

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        .center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 Error</title>
+</head>
+<body>
+
+        <img src="{{asset('custom_img/GFL_404error.gif')}}" alt="Paris" class="center">
+    
+</body>
+</html>
